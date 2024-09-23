3482674b456d3b3ef0bc8b55a587832ab58a852d
Joao Gabriel <gjoao7266@gmail.com>
2024-09-23 16:45:00 -0300

    Exceptions and FindById

---
+++ src/main/java/com/joaogabriel/workshopmongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.joaogabriel.workshopmongo.resources.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.joaogabriel.workshopmongo.service.exceptions.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandartError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandartError err = new StandartError(System.currentTimeMillis(), status.value(), "Object not found", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
